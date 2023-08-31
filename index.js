5b6a446fc3c2d8e2880afffca4ff86a55d9631ff
Max Gaillard <maxgaillar@gmail.com>
2023-08-31 11:10:55 -0400

    Installed cors package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const compression = require('compression')
 const connectDB = require('./config/db')
 require('dotenv').config()
@@ -8,19 +9,21 @@ const PORT = process.env.PORT || 5001
 connectDB()
 
 // Middlewares
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    req.header("Access-Control-Allow-Methods", "*")
-    next()
-})
-
-// app.use(compression())
+app.use(cors({ origin: '*', credentials: true }))
 
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
+
 const usersRouter = require('./routes/users.js')
 
-app.use('/api/users', usersRouter)
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//     req.header("Access-Control-Allow-Methods", "*")
+//     next()
+// })
+
+// app.use(compression())
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
--- routes/users.js
+++ routes/users.js
@@ -38,7 +38,7 @@ router.post('/login', async (req, res) => {
         //     return res.status(400).json({ errors: [{ message: 'Wrong password' }] })
         // }
 
-        res.status(200).json({ data: user })
+        res.status(201).json({ data: user })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
